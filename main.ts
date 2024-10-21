922ce8ea8c9e2eda4164c44b257a08ffe48d4403
kznpete <naoki.peter@edu.zh.ch>
2024-10-21 17:19:45 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,28 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (hero.vy == 0) {
+        hero.vy = -150
+    }
+})
+let hero: Sprite = null
+hero = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(hero)
+tiles.setCurrentTilemap(tilemap`Level0`)
+hero.ay = 300
+controller.moveSprite(hero, 100, 0)
